f659a1e73d7448dd94337cf2c735ccba93c866dd
Marinebedouret <marine.galloo@gmail.com>
2021-06-18 15:37:02 +0200

    Ajout dans la route inscription de la sécurité du mot de passe

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
+const passwordValidatorSchema = require('../middleware/password');
 
 
-router.post('/signup',  userCtrl.signup);
+
+router.post('/signup',passwordValidatorSchema ,userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
